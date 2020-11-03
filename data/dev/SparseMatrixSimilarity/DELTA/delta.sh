9c21dda98a937d898af81bcb32bf6198ddc4ad14
escobar2018 <antoine.escobar@ensiie.fr>
2020-11-03 20:12:12 +0100

    OK pour transfert responsab maj au conteneur

--- data/dev/SparseMatrixSimilarity/DELTA/delta.sh
+++ data/dev/SparseMatrixSimilarity/DELTA/delta.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
-killServerPID () {
-    ps aux | grep "python3 ../../../../app/dev/full_model/server_DF.py" | head -n 1 | awk '{print $2}' | xargs kill -9
+run_maj () {
+    # recupere le modele actuel
+    cp -r ../full_model2/ .
+
+    # execute la mise a jour du modele
+    python3 maj_model.py
+
+    # remplace le modele par sa mise-a-jour
+    cp -a ./full_model_buffer/. ../full_model2/
+
+    # sauvegarde modele de la veille
+    veille="modele_j-1.zip"
+    if [ -f "$veille" ]
+    then
+        rm $veille
+    fi
+    zip -r modele_j-1.zip ./full_model2
+
+    # libere espace disque (supprime fichiers en preservant dossiers enfants)
+    find ./full_model_buffer/ -type f -delete 
+    find ./full_model2/ -type f -delete
+
+    # autorise le modele en lecture/ecriture
+    chmod -R 777 ../full_model2
 }
 
-getServerPID () {
-    while read -r line
-    do
-        pid_to_stop=$line
-    done < server_PID
-}
-
-# recupere le modele actuel
-cp -r ../full_model2/ .
-
-# execute la mise à jour du modele
-python3 maj_model.py
-
-# remplace le modele par sa mise-a-jour
-cp -a ./full_model_buffer/. ../full_model2/
-
-# libère espace disque
-find ./full_model_buffer/ -type f -delete 
-find ./full_model2/ -type f -delete
-
-# autorise le modèle en lecture/ecriture
-chmod -R 777 ../full_model2
-
-touch ../full_model2/zorototo
+# s'execute tous les jours a 01h00 du matin
+while true
+do  
+    timy=$(date +%H)
+    if [ "$timy" -eq 01 ] 
+    then
+        run_maj
+        sleep 1h 1m
+    fi 
+done
